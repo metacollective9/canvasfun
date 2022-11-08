9071beb608535123b8e51fce5f4cae40f5cc2f07
MetaCollective <metacollective9@gmail.com>
2022-11-08 14:11:04 +0000

    Added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Layout from '../components/layout'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/layout";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Layout><Component {...pageProps} /></Layout>
+  return (
+    <Layout>
+      <Component {...pageProps} />
+      <Analytics />
+    </Layout>
+  );
 }
