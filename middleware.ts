19349170e8ed711ebeb010e43e60710f24e3ee1c
MetaCollective <metacollective9@gmail.com>
2022-11-09 14:33:58 +0000

    Added blogs page

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 export async function middleware(req: any, _ev:any) {
     const { pathname } = req.nextUrl
     if (pathname === '/') {
-        return NextResponse.redirect(new URL('/colorpicker', req.url))
+        return NextResponse.redirect(new URL('/posts', req.url))
     }
     return NextResponse.next()
 }
